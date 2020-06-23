89771337f76a14f18e82806177d8a64a2e7d81ac
Pedro Pablo Pastene <pe.pastene@gmail.com>
2020-06-23 00:34:08 -0400

    Change of port server and implementation of CORS

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const db = require('./models');
+const cors = require('cors')
 require('dotenv').config()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 const auth = require('./routes/auth')
 const todos = require('./routes/todos')
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use('/', auth);
 app.use('/todos', todos);
 
